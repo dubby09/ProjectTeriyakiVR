a1976e35014e49612f34e185b1edd3824296eea0
icebit <epicpotatopro@gmail.com>
2024-04-03 19:38:24 -0500

    Squirt bottle version 1

---
+++ Assets/Custom/Scripts/TeriyakiBottle.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR;
+
+// TODO: Integrate this with the other input system?
+public class TeriyakiBottle : MonoBehaviour
+{
+    private bool lastButtonState = false;
+    private List<InputDevice> devicesWithPrimaryButton;
+
+    public ParticleSystem particles;
+
+    private void Awake()
+    {
+        devicesWithPrimaryButton = new List<InputDevice>();
+    }
+
+    void OnEnable()
+    {
+        List<InputDevice> allDevices = new List<InputDevice>();
+        InputDevices.GetDevices(allDevices);
+        foreach (InputDevice device in allDevices)
+            InputDevices_deviceConnected(device);
+
+        InputDevices.deviceConnected += InputDevices_deviceConnected;
+        InputDevices.deviceDisconnected += InputDevices_deviceDisconnected;
+    }
+
+    private void OnDisable()
+    {
+        InputDevices.deviceConnected -= InputDevices_deviceConnected;
+        InputDevices.deviceDisconnected -= InputDevices_deviceDisconnected;
+        devicesWithPrimaryButton.Clear();
+    }
+
+    private void InputDevices_deviceConnected(InputDevice device)
+    {
+        bool discardedValue;
+        if (device.TryGetFeatureValue(CommonUsages.primaryButton, out discardedValue))
+        {
+            devicesWithPrimaryButton.Add(device); // Add any devices that have a primary button.
+        }
+    }
+
+    private void InputDevices_deviceDisconnected(InputDevice device)
+    {
+        if (devicesWithPrimaryButton.Contains(device))
+            devicesWithPrimaryButton.Remove(device);
+    }
+
+    void Update()
+    {
+        bool tempState = false;
+        foreach (var device in devicesWithPrimaryButton)
+        {
+            bool primaryButtonState = false;
+            tempState = device.TryGetFeatureValue(CommonUsages.primaryButton, out primaryButtonState) // did get a value
+                        && primaryButtonState // the value we got
+                        || tempState; // cumulative result from other controllers
+        }
+
+        if (tempState != lastButtonState) // Button state changed since last frame
+        {
+            FireBottle();
+            lastButtonState = tempState;
+        }
+    }
+
+    public void FireBottle()
+    {
+        Debug.Log("Particles");
+        particles.Play();
+    }
+}
